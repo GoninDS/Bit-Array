f8284d4577431096c5f2a5995574265c6a00c3d1
GoninDS <luisdi2509@gmail.com>
2023-02-17 15:16:07 -0600

    Initial functions for struct

--- src/boolean_bit_array.h
+++ src/boolean_bit_array.h
@@ -2,6 +2,22 @@
 #define BOOLEAN_BIT_ARRAY
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stdint.h>
+
+struct bit_array {
+  /// @brief Contains the array that holds each byte of information
+  uint8_t * array;
+};
+
+void bit_array_init(struct bit_array * array, uint64_t size);
+
+void bit_array_free(struct bit_array * array);
+
+void bit_array_set_true(struct bit_array * array, uint64_t index);
+
+void bit_array_set_false(struct bit_array * array, uint64_t index);
+
+bool bit_array_get_value(struct bit_array * array, uint64_t index);
 
 #endif  // BOOLEAN_BIT_ARRAY
