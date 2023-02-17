9330882bbf83c64d943d27a5b5060760c9868ac5
GoninDS <luisdi2509@gmail.com>
2023-02-17 15:27:15 -0600

    General structure in .c file

--- src/boolean_bit_array.c
+++ src/boolean_bit_array.c
@@ -1,1 +1,21 @@
 #include "boolean_bit_array.h"
+
+void bit_array_init(struct bit_array * array, uint64_t byte_amount) {
+
+}
+
+void bit_array_free(struct bit_array * array) {
+
+}
+
+void bit_array_set_true(struct bit_array * array, uint64_t index) {
+
+}
+
+void bit_array_set_false(struct bit_array * array, uint64_t index) {
+
+}
+
+bool bit_array_get_value(struct bit_array * array, uint64_t index) {
+  return true;
+}
