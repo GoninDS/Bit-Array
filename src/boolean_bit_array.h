2034dab0bd483adb8597f2cfd68077b785dc615d
GoninDS <luisdi2509@gmail.com>
2023-02-17 15:21:01 -0600

    Documentation for each function

--- src/boolean_bit_array.h
+++ src/boolean_bit_array.h
@@ -10,14 +10,45 @@ struct bit_array {
   uint8_t * array;
 };
 
-void bit_array_init(struct bit_array * array, uint64_t size);
+/**
+ * @brief Initializes every value in the bit array as false
+ * 
+ * @param array Pointer to the struct holding the array
+ * @param byte_amount Amount of bytes to allocate for the array
+ */
+void bit_array_init(struct bit_array * array, uint64_t byte_amount);
 
+/**
+ * @brief Free the memory allocated by the bit array
+ * 
+ * @param array Pointer to the struct holding the array
+ */
 void bit_array_free(struct bit_array * array);
 
+/**
+ * @brief Set true the bit corresponding to the desired index
+ * 
+ * @param array Pointer to the struct holding the array
+ * @param index Desired position to set true
+ */
 void bit_array_set_true(struct bit_array * array, uint64_t index);
 
+/**
+ * @brief Set false the bit corresponding to the desired index
+ * 
+ * @param array Pointer to the struct holding the array
+ * @param index Desired position to set false
+ */
 void bit_array_set_false(struct bit_array * array, uint64_t index);
 
+/**
+ * @brief Returns the corresponding value in the desired position
+ * 
+ * @param array Pointer to the struct holding the array
+ * @param index Desired position
+ * @return true If the value is set to true
+ * @return false If the value is set to false
+ */
 bool bit_array_get_value(struct bit_array * array, uint64_t index);
 
 #endif  // BOOLEAN_BIT_ARRAY
